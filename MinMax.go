646df8605d0b759dfdfb398a18d397bf404615b1
unknown <aswd25@yandex.ru>
2024-06-07 09:48:08 +0300

    add second fix MinMAx task

--- MinMax.go
+++ MinMax.go
@@ -3,14 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	arr := []int{2334454,5}
+	arr := []int{-5, -456465}
 
 	fmt.Println(MinMax(arr))
 
 }
 
 func MinMax(arr []int) [2]int {
-	var vMin, vMax int = arr[0], 1
+	var vMin, vMax int = arr[0], arr[0]
 
 	for _, v := range arr{
 		if v < vMin {
